25f6c91efc9e21a244bf3ccc0263ad1e277e3602
Asif Mujawar <asiflpnmujawar@gmail.com>
2024-06-19 00:37:42 +0530

    added error handling

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,81 +1,93 @@
 import { dummyData } from "@/data/tableData";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const status = searchParams.get("status");
-  const page = Number(searchParams.get("page")) || 1;
-  const limit = Number(searchParams.get("size")) || 10;
-  const startDateString = searchParams.get("startdate");
-  const endDateString = searchParams.get("enddate");
-  const firstName = searchParams.get("firstname");
-  const serviceName = searchParams.get("servicename");
+  try {
+    const { searchParams } = new URL(request.url);
+    const status = searchParams.get("status");
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("size")) || 10;
+    const startDateString = searchParams.get("startdate");
+    const endDateString = searchParams.get("enddate");
+    const firstName = searchParams.get("firstname");
+    const serviceName = searchParams.get("servicename");
 
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
-  let data = dummyData;
+    let data = dummyData;
 
-  const now = new Date();
-  const last24Hours = new Date(now.getTime() - 24 * 60 * 60 * 1000);
-  const normalizeDate = (date: Date) => {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-  };
-  const newRecords = data.filter((d) => {
-    const createdOnDate = normalizeDate(new Date(d.createdOn));
-    return createdOnDate >= last24Hours && createdOnDate <= now;
-  });
+    const now = new Date();
+    const last24Hours = new Date(now.getTime() - 24 * 60 * 60 * 1000);
+    const normalizeDate = (date: Date) => {
+      return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    };
+    const newRecords = data.filter((d) => {
+      const createdOnDate = normalizeDate(new Date(d.createdOn));
+      return createdOnDate >= last24Hours && createdOnDate <= now;
+    });
 
-  switch (status) {
-    case "all":
-      break;
+    switch (status) {
+      case "all":
+        break;
 
-    case "lead":
-      data = data.filter((d) => d.status === "lead");
-      break;
+      case "lead":
+        data = data.filter((d) => d.status === "lead");
+        break;
 
-    case "bydaterange":
-      const sDate = normalizeDate(new Date(startDateString as string));
-      const eDate = normalizeDate(new Date(endDateString as string));
-      data = data.filter((d) => {
-        const createdOnDate = normalizeDate(new Date(d.createdOn));
-        return createdOnDate >= sDate && createdOnDate <= eDate;
-      });
-      break;
+      case "bydaterange":
+        const sDate = normalizeDate(new Date(startDateString as string));
+        const eDate = normalizeDate(new Date(endDateString as string));
+        data = data.filter((d) => {
+          const createdOnDate = normalizeDate(new Date(d.createdOn));
+          return createdOnDate >= sDate && createdOnDate <= eDate;
+        });
+        break;
 
-    case "newlyadded":
-      data = newRecords;
-      break;
+      case "newlyadded":
+        data = newRecords;
+        break;
 
-    case "byfirstname":
-      data = data.filter((d) =>
-        d.payer.toLowerCase().includes(firstName?.toLowerCase() as string)
+      case "byfirstname":
+        data = data.filter((d) =>
+          d.payer.toLowerCase().includes(firstName?.toLowerCase() as string)
+        );
+        break;
+
+      case "byservicename":
+        data = data.filter((d) =>
+          d.service.name
+            .toLowerCase()
+            .includes(serviceName?.toLowerCase() as string)
+        );
+        break;
+      default:
+        break;
+    }
+
+    const pagiNatedData = data.slice(startIndex, endIndex);
+    return Response.json(
+      {
+        success: true,
+        data: pagiNatedData,
+        currentPage: page,
+        totalPages: Math.ceil(data.length / limit),
+        totalRecords: dummyData.length,
+        newRecords: newRecords.length,
+        limit,
+        leadTotalRecords: dummyData.filter((data) => data.status === "lead")
+          .length,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        { status: 200 }
       );
-      break;
-
-    case "byservicename":
-      data = data.filter((d) =>
-        d.service.name
-          .toLowerCase()
-          .includes(serviceName?.toLowerCase() as string)
-      );
-      break;
-    default:
-      break;
+    }
   }
-
-  const pagiNatedData = data.slice(startIndex, endIndex);
-  return Response.json(
-    {
-      success: true,
-      data: pagiNatedData,
-      currentPage: page,
-      totalPages: Math.ceil(data.length / limit),
-      totalRecords: dummyData.length,
-      newRecords: newRecords.length,
-      limit,
-      leadTotalRecords: dummyData.filter((data) => data.status === "lead")
-        .length,
-    },
-    { status: 200 }
-  );
 }
